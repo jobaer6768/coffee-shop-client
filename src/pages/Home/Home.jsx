80f6e6f23d0def80e5bce666465b02237af84921
Md Jobaer Hossain <120457646+jobaer6768@users.noreply.github.com>
2023-10-17 23:42:35 +0600

    able to load data from server

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,13 @@
+import { useLoaderData } from "react-router-dom";
 
 
 const Home = () => {
+
+    const loadedCoffees = useLoaderData();
+
     return (
         <div>
-            <h1 className="text-6xl text-red-400">Home</h1>
+            <h1 className="text-6xl text-red-400">{loadedCoffees.length}</h1>
         </div>
     );
 };
